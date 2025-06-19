5451440893d2616e6424c0f96b3966cb37a17955
Riya Shukla <riyaa.ssskl@gmail.com>
2025-06-19 19:07:23 +0530

    Create weather_api.py

---
+++ Virtual_Assistant_Calculator_Mini_Project1/weather_api.py
@@ -0,0 +1,16 @@
+python
+CopyEdit
+import requests
+
+def get_weather(city, api_key):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            'temperature': data['main']['temp'],
+            'humidity': data['main']['humidity'],
+            'wind_speed': data['wind']['speed']
+        }
+    else:
+        raise Exception("City not found or API error.")
